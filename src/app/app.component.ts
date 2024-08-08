3152eb283b1378d5756cb509f95a94f6e86fce04
Manuel Braun <manuel.braun.coder@gmail.com>
2024-08-08 20:48:14 +0200

    generate log in, signup screen

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from "./shared/header/header.component";
 import { NavBarComponent } from "./shared/nav-bar/nav-bar.component";
@@ -11,6 +11,20 @@ import { NavBarComponent } from "./shared/nav-bar/nav-bar.component";
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'join-2.0';
+
+  onRootPath: boolean = false;
+
+
+  ngOnInit(): void {
+    console.log(window.location.pathname);
+    if (window.location.pathname === '/') {
+      console.log('on /');
+      this.onRootPath = true;
+      
+    } else {
+      this.onRootPath = false;
+    }
+  }
 }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,8 +3,10 @@ import { SummaryComponent } from './summary/summary.component';
 import { AddTaskComponent } from './add-task/add-task.component';
 import { BoardComponent } from './board/board.component';
 import { ContactsComponent } from './contacts/contacts.component';
+import { LoginComponent } from './login/login.component';
 
 export const routes: Routes = [
+    { path: '', component: LoginComponent},
     { path: 'summary', component: SummaryComponent},
     { path: 'addTask', component: AddTaskComponent},
     { path: 'board', component: BoardComponent},
