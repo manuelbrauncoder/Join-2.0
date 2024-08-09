481c97a3520b6ad25d8bf310730b4161f81d9c8a
Manuel Braun <manuel.braun.coder@gmail.com>
2024-08-09 07:33:34 +0200

    implement router event, hide header and navbar on login screen

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,30 +1,38 @@
-import { Component, inject, OnInit } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-import { HeaderComponent } from "./shared/header/header.component";
-import { NavBarComponent } from "./shared/nav-bar/nav-bar.component";
-
+import { Component } from '@angular/core';
+import { RouterOutlet, Router, Event, NavigationEnd } from '@angular/router';
+import { HeaderComponent } from './shared/header/header.component';
+import { NavBarComponent } from './shared/nav-bar/nav-bar.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [RouterOutlet, HeaderComponent, NavBarComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
-export class AppComponent implements OnInit {
+export class AppComponent {
   title = 'join-2.0';
 
-  onRootPath: boolean = false;
+  onLogin: boolean = false;
 
+  constructor(private router: Router) {
+    this.router.events.subscribe((event: Event) => {
+      if (event instanceof NavigationEnd) {
+        console.log('On Route:', event.url);
+        this.checkPath(event.url);
+      }
+    });
+  }
 
-  ngOnInit(): void {
-    console.log(window.location.pathname);
-    if (window.location.pathname === '/') {
-      console.log('on /');
-      this.onRootPath = true;
-      
+  /**
+   * hide header and nav-bar if on Login Screen
+   * @param url current route
+   */
+  checkPath(url: string) {
+    if (url === '/') {
+      this.onLogin = true;
     } else {
-      this.onRootPath = false;
+      this.onLogin = false;
     }
   }
 }
