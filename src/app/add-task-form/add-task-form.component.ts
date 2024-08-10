8071fb07eb824037d92593bb9fdc890dc848de98
Manuel Braun <manuel.braun.coder@gmail.com>
2024-08-10 21:25:06 +0200

    push users to assigned to in task

--- src/app/add-task-form/add-task-form.component.ts
+++ src/app/add-task-form/add-task-form.component.ts
@@ -1,19 +1,55 @@
 import { Component, inject } from '@angular/core';
 import { Task } from '../models/task.class';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { TaskService } from '../services/task.service';
+import { UserService } from '../services/user.service';
+import { InitialsPipe } from '../pipes/initials.pipe';
 
 @Component({
   selector: 'app-add-task-form',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, FormsModule, InitialsPipe],
   templateUrl: './add-task-form.component.html',
   styleUrl: './add-task-form.component.scss'
 })
 export class AddTaskFormComponent {
+    taskService = inject(TaskService);
+    userService = inject(UserService);
     task = new Task();
+    dueDate: Date | null = null;
+    showOptions: boolean = false;
+
 
   constructor(){ }
 
+  asignUserToTask(user: string){
+    this.task.assignedTo.push(user);
+  }
+
   changeTaskPrio(prio: 'urgent' | 'medium' | 'low'){
     this.task.priority = prio;
+  }
+
+  changeTaskCategory(cat: 'User Story' | 'Technical Task'){
+    this.task.category = cat;
+  }
+
+  handleDueDate(){
+    if (this.dueDate) {
+      const date = typeof this.dueDate === 'string' ? new Date(this.dueDate) : this.dueDate;
+      const timestamp = date.getTime();
+      
+      if (!isNaN(timestamp)) {
+        this.task.dueDate = timestamp;
+      } else {
+        console.error('Invalid due date');
+      }
+    }
+  }
+
+  async saveTask() {
+    this.handleDueDate();
+    await this.taskService.fireService.addTask(this.task);
   }
 }
--- src/app/board-task-card-empty/board-task-card-empty.component.ts
+++ src/app/board-task-card-empty/board-task-card-empty.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-board-task-card-empty',
@@ -8,5 +8,30 @@ import { Component } from '@angular/core';
   styleUrl: './board-task-card-empty.component.scss'
 })
 export class BoardTaskCardEmptyComponent {
+  @Input()status: string = '';
 
+
+
+  getStatusString(): string {
+    switch (this.status) {
+      case 'todo':
+        return 'No tasks To do';
+      case 'progress':
+        return 'No tasks in progress';
+      case 'feedback':
+        return 'No tasks for feedback';
+      case 'done':
+        return 'No tasks done';
+      default:
+        return 'todo';
+    }
+  }
 }
+
+// create custom strings for different states:
+// No tasks To do
+// No tasks in progress
+// No tasks for feedback
+// No tasks done
+
+
