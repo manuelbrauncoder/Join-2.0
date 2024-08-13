135e567767fa3e708a53710f07b2f15c9bb2f482
Manuel Braun <manuel.braun.coder@gmail.com>
2024-08-13 11:41:32 +0200

    style contact detail card

--- src/app/contacts-detail-view/contacts-detail-view.component.ts
+++ src/app/contacts-detail-view/contacts-detail-view.component.ts
@@ -1,15 +1,18 @@
-import { Component, Input } from '@angular/core';
+import { Component, inject, Input } from '@angular/core';
 import { User } from '../models/user.class';
+import { InitialsPipe } from '../pipes/initials.pipe';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-contacts-detail-view',
   standalone: true,
-  imports: [],
+  imports: [InitialsPipe, CommonModule],
   templateUrl: './contacts-detail-view.component.html',
   styleUrl: './contacts-detail-view.component.scss'
 })
 export class ContactsDetailViewComponent {
-  
+
+
   @Input() currentUser: User = {
     name: '',
     id: '',
