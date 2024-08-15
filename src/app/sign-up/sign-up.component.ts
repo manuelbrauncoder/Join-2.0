69873282bbcf9556d98c056f393ff09e3df4dc8d
Manuel Braun <manuel.braun.coder@gmail.com>
2024-08-15 16:51:36 +0200

    start with sign up form

--- src/app/sign-up/sign-up.component.ts
+++ src/app/sign-up/sign-up.component.ts
@@ -1,16 +1,38 @@
-import { Component, EventEmitter, Output } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { UserCl } from '../models/user.class';
+import { FormsModule, NgForm } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-sign-up',
   standalone: true,
-  imports: [],
+  imports: [FormsModule, CommonModule],
   templateUrl: './sign-up.component.html',
   styleUrl: './sign-up.component.scss'
 })
-export class SignUpComponent {
+export class SignUpComponent implements OnInit {
   @Output() signUp = new EventEmitter<boolean>();
+
+  newUser: UserCl = new UserCl;
+  passwordConfirmation: string = '';
+  policyCheck: boolean = false;
 
   hideSignUp(){
     this.signUp.emit(false);
+  }
+
+ async onSubmit(ngForm: NgForm){
+    if (ngForm.valid && ngForm.submitted && this.confirmPassword()) {
+      console.log(this.newUser);
+      
+    }
+  }
+
+  confirmPassword(){
+    return this.newUser.password === this.passwordConfirmation;
+  }
+
+  ngOnInit(): void {
+    this.policyCheck = false;
   }
 }
